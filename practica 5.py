b78d992539e7694dc2fafa92f08628a5f0179d58
ArturoEn <61199596+ArturoEn@users.noreply.github.com>
2020-03-12 09:49:51 -0700

    practica 5, suma o multiplicacion
    en esta practica se sumó o multiplicó, dos números dependiendo la condicional

--- practica 5.py
+++ practica 5.py
@@ -1,7 +1,7 @@
 # Programa que suma dos numeros
 a = float (input("ingresa un numero: "))
 b = float (input("ingresa otro nuero: "))
-if a >0 and b > 0:
+if a >0 or b > 0:
   c = a + b
 else:
   c = a * b
