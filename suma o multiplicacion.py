cf622207c983d357b484e63606663f32120fe56a
ArturoEn <61199596+ArturoEn@users.noreply.github.com>
2020-03-12 09:45:06 -0700

    Add files via upload

---
+++ suma o multiplicacion.py
@@ -0,0 +1,8 @@
+# Programa que suma dos numeros
+a = float (input("ingresa un numero: "))
+b = float (input("ingresa otro nuero: "))
+if a >0 and b > 0:
+  c = a + b
+else:
+  c = a * b
+print ("la suma es: ",c)
